1ff7b54817740460117665233f742bce4d17e6b1
Richit <bhatrichit10@gmail.com>
2025-12-21 15:50:55 +0530

    Test for CLI

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // RichPass - CLI based password managewr 
 // Made by Richit
 /* for hackvault */
+const yargs = require('yargs')
+const chalk = require('chalk')
+
+console.log('Test 1 : CLI working')
+
+
